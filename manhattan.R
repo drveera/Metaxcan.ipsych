e80ace41ef6510cdaa7fb9002ad7ab7f53558f26
veera_dr <drveeramanikandan@gmail.com>
2016-10-06 16:37:48 +0200

    added 1kg and hapmap db files

--- manhattan.R
+++ manhattan.R
@@ -29,15 +29,17 @@ gwas$chrom <- factor(gwas$chrom, levels = 1:22)
 gwas <- gwas[order(gwas$chrom, gwas$bp),]
 gwas$index <- 1:nrow(gwas)
 
+sigline <- -log10(0.05/nrow(gwas))
+
 labelthreshold <- args[3]
 if (is.na(labelthreshold)){
-    sigline <- -log10(0.05/nrow(gwas))
+    labelline <- -log10(0.05/nrow(gwas))
 } else {
-    sigline <- as.numeric(labelthreshold)
+    labelline <- as.numeric(labelthreshold)
 }
 
 gwas$label <- gwas$gene_name
-gwas$label <- replace(gwas$label, -log10(gwas$pvalue) < sigline, NA)
+gwas$label <- replace(gwas$label, -log10(gwas$pvalue) < labelline, NA)
 
 gwas.split <- split(gwas,gwas$chrom)
 xbreaks <- sapply(gwas.split,function(x) x$index[round(nrow(x)/2)])
--- metaxcan.sh
+++ metaxcan.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-OPTS=`getopt -o h -l out:,gwas:,nojob,pop:,test,cov:,db:,man,csv:,labelthreshold: -n 'metaxcan' -- "$@"`
+OPTS=`getopt -o h -l out:,gwas:,nojob,pop:,test,cov:,db:,man,csv:,labelthreshold:,tpop: -n 'metaxcan' -- "$@"`
 
 eval set -- "$OPTS"
 
@@ -21,6 +21,7 @@ while true; do
 	--man) man=true; shift ;;
 	--csv) csv=$2; shift 2;;
 	--labelthreshold) labelthreshold=$2; shift 2;;
+	--tpop) tpop=$2 shift 2;;
 	-h | --help) helpmessage; exit 1; shift ;;
 	--) shift; break ;;
 	*) exit 1 ;;
@@ -103,7 +104,7 @@ then
     while read i
     do
 	$wd/pipeline.sh $out $gwasbase.chfiles $wd/${i}_0.5.db $wd/${i}.txt.gz${pop}
-    done < $wd/weights.list${pop}
+    done < $wd/weights.list${tpop}
     
 else
     
@@ -111,7 +112,7 @@ while read i
 do
     echo "$wd/pipeline.sh $out $gwasbase.chfiles $wd/${i}_0.5.db $wd/${i}.txt.gz${pop}"
     
-done < $wd/weights.list${pop} > $out/$out.adispatch
+done < $wd/weights.list${tpop} > $out/$out.adispatch
 
 head -1 $out/$out.adispatch > $out/$out.job1.adispatch
 
