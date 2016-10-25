a68ad4969b98428a0566a400dbe2ee4bb410c571
veera_dr <drveeramanikandan@gmail.com>
2016-10-25 19:57:40 +0200

    edited write snplist rscript

--- covariances/write.snplist.R
+++ covariances/write.snplist.R
@@ -2,5 +2,6 @@ args <-  commandArgs(trailingOnly = TRUE)
 
 
 dfm <- read.csv(args[1], header = TRUE)
+dfm <- dfm[! duplicated(dfm$rsid),]
 
 write.table(dfm[,c("rsid","eff_allele")],paste0(args[1],".snp.list") ,sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
