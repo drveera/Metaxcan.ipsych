f533a960992bbd0e3f3d62143e07e578da8c630a
veera_dr <drveeramanikandan@gmail.com>
2016-10-25 23:39:51 +0200

    update

--- covariances/allele.check.R
+++ covariances/allele.check.R
@@ -4,10 +4,12 @@ args <- commandArgs(trailingOnly = TRUE)
 ##args1 bim file
 ##args2 list file
 
-bim <- read.table(args[1]) #4th column is a1 allele
+bim <- read.table(args[1])
+                                        #4th column is a1 allele
 names(bim) <- c("chrom","rsid","v3","pos","A1","A2")
 
-snp <- read.table(args[2]) #2nd column is a1 allele
+snp <- read.table(args[2])
+                                        #2nd column is a1 allele
 names(snp) <- c("rsid","a1")
 
 dfm <- merge(bim,snp,by="rsid")
