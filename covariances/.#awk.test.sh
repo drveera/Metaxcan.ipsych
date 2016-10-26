3ecf48206ab189811115d7f02a1e473708401633
veera_dr <drveeramanikandan@gmail.com>
2016-10-26 14:50:02 +0200

    update

--- covariances/.#awk.test.sh
+++
@@ -1,1 +0,0 @@
-veera@fe1.genomedk.net.20642:1467015645
--- covariances/awk.test.sh
+++ covariances/awk.test.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 awk '{ 
-printf $1"\t"
-for (i=1; i<=NF; i = i+3)
-printf $i + $(i+1) + $(i+2) "\t"
-printf "\n"
+printf chr$1"\t"$2"\t"$3"\t"$4"\t"$5"\t";
+for (i=6; i<=NF; i = i+3)
+printf sum/(NF-6)"\t"
+{
+if($(i+0) == 0 && $(i+1) == 0 && $(i+2) == 0) printf "\tNA"; 
+else printf "\t"$(i+0)*0+$(i+1)*1+$(i+2)*2
+}; printf "\n"
+
 }' $1
+
 
