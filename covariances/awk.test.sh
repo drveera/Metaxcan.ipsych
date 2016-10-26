9d404c8217933bbc9a2f0533d510fac675d807f8
veera_dr <drveeramanikandan@gmail.com>
2016-10-26 15:15:18 +0200

    update

--- covariances/awk.test.sh
+++ covariances/awk.test.sh
@@ -2,9 +2,8 @@
 
 awk '{ 
 printf chr$1"\t"$2"\t"$3"\t"$4"\t"$5"\t" sum;
-for (i=6; i<=NF; i = i+3; sum=sum+$i)
+for (i=6; i<=NF; i = i+3)
 {
-printf sum/(NF-6)*2 "\t"
 if($(i+0) == 0 && $(i+1) == 0 && $(i+2) == 0) printf "\tNA"; 
 else printf "\t"$(i+0)*0+$(i+1)*1+$(i+2)*2
 }; printf "\n"
