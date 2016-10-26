6a646be2baa5a9e819dbcc6387ea1ecec8fcf228
veera_dr <drveeramanikandan@gmail.com>
2016-10-26 11:36:29 +0200

    update

--- covariances/write.snp.report.R
+++ covariances/write.snp.report.R
@@ -5,6 +5,15 @@ args <- commandArgs(trailingOnly = TRUE)
 
                                         #args1 db csv file
                                         #args2 bim file
-db <- read.csv(args[1], header = TRUE)
+dbsnps <- read.csv(args[1], header = TRUE)
 
 bim <- read.table(args[2])
+names(bim) <- c("chrom","rsid","v3","pos","A1","A2")
+
+dbsnps.miss <- dbsnps[! dbsnps$rsid %in% bim$rsid,]
+
+write.table(dbsnps.miss, args[3], sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+
+
+
