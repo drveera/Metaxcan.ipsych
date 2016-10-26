3d06a9d75c110acccd7f8718e3a4e4806d476a28
veera_dr <drveeramanikandan@gmail.com>
2016-10-26 10:38:43 +0200

    update

--- covariances/write.snplist.R
+++ covariances/write.snplist.R
@@ -1,7 +1,10 @@
 args <-  commandArgs(trailingOnly = TRUE)
 
+                                        #args1 csv file
+                                        #args2 outname
+
 
 dfm <- read.csv(args[1], header = TRUE)
 dfm <- dfm[! duplicated(dfm$rsid),]
 
-write.table(dfm[,c("rsid","eff_allele")],paste0(args[1],".snp.list") ,sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
+write.table(dfm[,c("rsid","eff_allele")],args[2],sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
