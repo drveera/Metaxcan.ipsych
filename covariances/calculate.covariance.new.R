d15affdc70696d987d869666dedfa99463befd48
veera_dr <drveeramanikandan@gmail.com>
2016-10-27 13:31:44 +0200

    update

---
+++ covariances/calculate.covariance.new.R
@@ -0,0 +1,56 @@
+#!/bin/env Rscript
+
+                                        #args1 snp.file
+                                        #args2 dosage file
+                                        #args3 outname
+
+args <- commandArgs(trailingOnly = TRUE)
+
+                                        #libraries
+library(data.table)
+library(reshape2)
+library(parallel)
+
+cat("Reading snp files","\n")
+dblist.files <- readLines(args[1])
+                                        #snp <- read.table(args[1])
+
+                                        #names(snp) <- c("rsid","gene")
+#dblist <- list()
+#for (i in 1:length(dblist.files)){
+#    dblist[[i]] <- read.csv(dblist.files[i], header = TRUE)
+#}
+
+dbcsv <- read.csv(args[1], header = TRUE)
+
+cat("Reading dose file")
+dose <- fread(paste0("zcat ",args[2]))
+names(dose)[2] <- "rsid"
+
+dose.merge <- merge(db[,c("rsid","gene")], dose, by = "rsid")
+
+
+cal.cov <- function(dfm){
+    dose.sub.matrix <- t(dfm[,8:ncol(dfm)])
+    colnames(dose.sub.matrix) <- dose.sub$rsid
+    dose.cov.matrix <- cov(dose.sub.matrix, use = "na.or.complete")
+    dose.cov.melted <- melt(dose.cov.matrix)
+    names(dose.cov.melted) <- c("RSID1","RSID2","VALUE")
+    GENE <- rep(dfm$gene[1],nrow(dose.cov.melted))
+    return(cbind(GENE,dose.cov.melted))
+}
+
+
+
+dose.split <- split(dose.merge, dose.merge$rsid)
+
+doselst <- lapply(dose.split, cal.cov)
+doselst <- do.call(rbind,doselst)
+write.table(doselst,paste0(basename(args[1]),".covariance.matrix"),row.names = FALSE, col.names = FALSE, quote = FALSE)    
+
+
+
+
+
+
+
