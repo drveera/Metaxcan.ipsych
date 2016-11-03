3fcde6a66800879fe04a9fe9032084c54a1575d1
Veera <veera@hello908.local>
2016-11-03 21:55:18 +0100

    update

--- covariances/imputeNA.sh
+++ covariances/imputeNA.sh
@@ -2,8 +2,7 @@
 
 while IFS= read line
 do
-    maf=$(echo $line | cut -f 6 )
-    val=$(echo $maf*2 | bc -l)
-    echo $line | sed s/NA/$val/g
+    maf=$(echo $line | cut -f 6 -d " ")
+    navalue=$(echo $maf*2 | bc -l)
+    echo $line | sed s/NA/$navalue/g 
 done < $1
-
