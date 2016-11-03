31ca89ccf83fa7c848ac38053b97b2bd1cf5ec1a
Veera <veera@hello908.local>
2016-11-03 21:30:13 +0100

    added imputeNA script

--- covariances/calculate.covariance.new.R
+++ covariances/calculate.covariance.new.R
@@ -9,17 +9,8 @@ args <- commandArgs(trailingOnly = TRUE)
                                         #libraries
 library(data.table)
 library(reshape2)
-library(parallel)
 
 
-
-                                        #snp <- read.table(args[1])
-
-                                        #names(snp) <- c("rsid","gene")
-#dblist <- list()
-#for (i in 1:length(dblist.files)){
-#    dblist[[i]] <- read.csv(dblist.files[i], header = TRUE)
-#}
 
 cat("reading csv file \n")
 dbcsv <- read.csv(args[1], header = TRUE)
@@ -33,8 +24,6 @@ dose.merge <- merge(dbcsv[,c("rsid","gene")], dose, by = "rsid")
 cat("the class of merge data fame ", class(dose.merge))
 
 
-
-
 cal.cov <- function(dfm){
     dose.sub.matrix <- t(dfm[,8:ncol(dfm)])
     colnames(dose.sub.matrix) <- dfm$rsid
@@ -46,37 +35,14 @@ cal.cov <- function(dfm){
 }
 
 
-
-#cat("splitting the data frame by gene \n")
-#dose.split <- split(dose.merge, dose.merge$rsid)
-
-#cat("applying the cal.cov function \n")
-#doselst <- mclapply(dose.split,cal.cov,mc.cores = 4)
-#doselst <- lapply(dose.split, cal.cov)
-
 library(dplyr)
 
-newdose <- apply(dose.merge,1,function(x) return(gsub("NA",as.numeric(x[6])*2,x)))
-newdose <- as.data.frame(t(newdose))
-
-newdose_name=gsub(".gz","",args[2])
-write.table(newdose,newdose_name,row.names = FALSE, col.names = FALSE, quote = FALSE)
-system(paste0("rm ",args[2]))
-system(paste0("gzip ",newdose_name))
-
-doselst <- newdose %>%
+cat("calculating cov \n")
+doselst <- dose.merge %>%
     group_by(gene) %>%
     do(cal.cov(.))
 
 cat("done! \n")
-
-print(class(doselst))
-print(head(doselst))
-print(dim(doselst))
-
-
-#cat("merging back")
-#doselst <- do.call(rbind,doselst)
 
 cat("writing the file")
 write.table(doselst,args[3],row.names = FALSE, col.names = FALSE, quote = FALSE)
