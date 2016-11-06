1ea9ceb2feed1ac05bdd5d5dece651f6cffdacc3
Veera <veera@hello908.local>
2016-11-06 21:07:53 +0100

    update

--- covariances/write.snplist.R
+++ covariances/write.snplist.R
@@ -6,5 +6,7 @@ args <-  commandArgs(trailingOnly = TRUE)
 library(data.table)
 dfm <- fread(args[1], header = TRUE)
 
+dfm <- dfm[!duplicated(rsid)]
+
 
 write.table(dfm[,c("rsid","eff_allele"),with=FALSE],args[2],sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
