23dbc34c6793ae86714c1252fdfd24087842ddcc
veera_dr <drveeramanikandan@gmail.com>
2016-11-07 14:31:55 +0100

    added summary format script

---
+++ covariances/convert.dosage.R
@@ -0,0 +1,11 @@
+#!/bin/env Rscript
+
+
+args <- commandArgs(trailingOnly = TRUE)
+
+#load the reference file(rsid allele1 allele2) 
+##this is compressed rdata file for easy load
+load("")
+
+##read the dose
+
---
+++ covariances/format.plink.summary.R
@@ -0,0 +1,40 @@
+#!/bin/env Rscript
+
+args <- commandArgs(trailingOnly = TRUE)
+
+
+##libraries
+library(data.table)
+
+##read the summary file
+
+assoc <- fread(args[1], header = TRUE)
+
+assoc <- assoc[assoc$TEST == "ADD",]
+names(assoc)[2] <- "rsid"
+
+## read the bim file to get allele information
+
+db <- fread(args[2], sep = ",", header = TRUE)
+
+
+dfm <- merge(assoc,db,by = "rsid")
+
+dfm <- dfm[,c("CHR","rsid","A1","ref_allel","OR","P")]
+
+print(table(dfm$A1 == dfm$effect_allele))
+
+dfm.split <- split(dfm,dfm$CHR)
+
+for (i in 1:length(dfm.split)){
+    fwrite(dfm.split[[i]],paste0(args[3],".chr",i,"sum"), sep = "\t", row.names = FALSE, quote = FALSE)
+}
+
+
+
+
+
+
+
+
+
